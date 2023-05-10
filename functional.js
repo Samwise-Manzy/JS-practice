702ef03a964564de888dd022272fecc17af72b63
Samuel Manzanares <samuel.manz57@gmail.com>
2023-05-10 08:26:16 -0500

    function programming finished

--- functional.js
+++ functional.js
@@ -572,3 +572,33 @@ function sentensify(str) {
   sentensify("May-the-force-be-with-you");
 
 
+// // ***************************************************
+// Apply Functional Programming to Convert Strings to URL Slugs
+
+// Fill in the urlSlug function so it converts a string title and returns the hyphenated version for the URL. You can use any of the methods covered in this section, and don't use replace. Here are the requirements:
+
+// The input is a string with spaces and title-cased words
+
+// The output is a string with the spaces between words replaced by a hyphen (-)
+
+// The output should be all lower-cased letters
+
+// The output should not have any spaces
+
+// Only change code below this line
+function urlSlug(title) {
+
+    let titleCopy = title.slice().toLowerCase().split(" ");
+
+    let urlTitle = titleCopy.filter( char => {
+      if (char != " ") {
+        return char
+      }
+    }).join("-")
+    
+    return urlTitle;
+
+}
+// Only change code above this line
+// console.log(urlSlug("A Mind Needs Books Like A Sword Needs A Whetstone"));
+
